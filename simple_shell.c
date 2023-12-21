95d560695617b23e5076606faa94486b028f3b93
Yahsai <yahsaisantana1@gmail.com>
2023-12-20 20:17:49 -0400

    new

--- simple_shell.c
+++ simple_shell.c
@@ -5,24 +5,24 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+extern char **environ;
+
 #define MAX_COMMAND_LENGTH 100
 #define MAX_ARGS 10
-
-/* Additional functions if you have them */
 
 /**
  * is_builtin_command - determines whether given command is built-in or not
  * @command: the given command
- * Return: 0 if not built-in, 1 if built-in
+ * Return: 1 if built-in, 0 if not
  */
 int is_builtin_command(char *command)
 {
-	if (strcmp(command, "exit") == 0 || strcmp(command, "clear") == 0 ||
-			strcmp(command, "cd") == 0 || strcmp(command, "env") == 0)
-	{
-		return 1;
-	}
-	return 0;
+    if (strcmp(command, "exit") == 0 || strcmp(command, "clear") == 0 ||
+        strcmp(command, "cd") == 0 || strcmp(command, "env") == 0)
+    {
+        return 1;
+    }
+    return 0;
 }
 
 /**
@@ -32,11 +32,11 @@ int is_builtin_command(char *command)
  */
 void print_environment(char **env)
 {
-	while (*env != NULL)
-	{
-		printf("%s\n", *env);
-		env++;
-	}
+    while (*env != NULL)
+    {
+        printf("%s\n", *env);
+        env++;
+    }
 }
 
 /**
@@ -46,121 +46,149 @@ void print_environment(char **env)
  */
 void prompt(int signo)
 {
-	(void)signo;  /* Ignore the unused parameter to avoid a warning */
-	printf("\n");
-	fflush(stdout);
+    (void)signo; /* Ignore the unused parameter to avoid a warning */
+    printf("\n");
+    fflush(stdout);
 }
-int main(int ac, char **av, char **env)
+
+int main(void)
 {
-	char input[MAX_COMMAND_LENGTH];
-	int status;
-	pid_t pid;
-	char *tmp_av[MAX_ARGS + 1];
-	char *token;
-	int has_token;
-	int last_exit_status = 0;
+    char input[MAX_COMMAND_LENGTH];
+    int status;
+    pid_t pid;
+    char *tmp_av[MAX_ARGS + 1];
+    char *token;
+    int ac;
+    int has_token;
+    int last_exit_status = 0;
 
-	signal(SIGINT, prompt);
+    ssize_t bytesRead;  /*  Move the declaration here */
 
-	while (1)
-	{
-		write(1, "$ ", 2);
-		fflush(stdout);
+    signal(SIGINT, prompt);
 
-		if (fgets(input, sizeof(input), stdin) == NULL)
-		{
-			break;
-		}
+    while (1)
+    {
+        char buffer[1024];
 
-		input[strcspn(input, "\n")] = '\0';
+        write(STDOUT_FILENO, "$ ", 2);
+        fflush(stdout);
 
-		if (strlen(input) == 0)
-		{
-			continue;
-		}
+        if (fgets(input, sizeof(input), stdin) == NULL)
+        {
+            break;
+        }
 
-		token = strtok(input, " ");
-		ac = 0;
-		has_token = 0;
+        input[strcspn(input, "\n")] = '\0';
 
-		while (token != NULL && ac < MAX_ARGS)
-		{
-			tmp_av[ac++] = token;
-			has_token = 1;
-			token = strtok(NULL, " ");
-		}
+        if (strlen(input) == 0)
+        {
+            continue;
+        }
 
-		if (!has_token)
-		{
-			continue;
-		}
+        token = strtok(input, " ");
+        ac = 0;
+        has_token = 0;
 
-		tmp_av[ac] = NULL;
+        while (token != NULL && ac < MAX_ARGS)
+        {
+            tmp_av[ac++] = token;
+            has_token = 1;
+            token = strtok(NULL, " ");
+        }
 
-		if (strcmp(tmp_av[0], "exit") == 0)
-		{
-			last_exit_status = 0;
-			break;
-		}
+        if (!has_token)
+        {
+            continue;
+        }
 
-		if (is_builtin_command(tmp_av[0]))
-		{
-			if (strcmp(tmp_av[0], "cd") == 0)
-			{
-				if (ac > 1)
-				{
-					if (chdir(tmp_av[1]) != 0)
-					{
-						perror("cd");
-						last_exit_status = 1;
-					}
-				}
-			}
-			else if (strcmp(tmp_av[0], "env") == 0)
-			{
-				print_environment(env);
-			}
-		}
-		else
-		{
-			int i;
-			for (i = 0; i < ac; i++)
-			{
-				av[i] = tmp_av[i];
-			}
-			av[ac] = NULL;
+        tmp_av[ac] = NULL;
 
-			pid = fork();
-			if (pid < 0)
-			{
-				perror("fork");
-				last_exit_status = 1;
-				exit(EXIT_FAILURE);
-			}
-			else if (pid == 0)
-			{
-				if (execvp(av[0], av) == -1)
-				{
-					perror("execvp");
-					last_exit_status = 127;
-					exit(EXIT_FAILURE);
-				}
-			}
-			else
-			{
-				if (wait(&status))
-				{
-					 (WIFEXITED(status));
-					 {
-						last_exit_status = WEXITSTATUS(status);
-					 }
-				}
-					 else
-					 {
-						last_exit_status = 1;
-				}
-			}
-		}
-	}
-	return (last_exit_status);
+        if (strcmp(tmp_av[0], "exit") == 0)
+        {
+            last_exit_status = 0;
+            break;
+        }
+
+        if (is_builtin_command(tmp_av[0]))
+        {
+            if (strcmp(tmp_av[0], "cd") == 0)
+            {
+                if (ac > 1)
+                {
+                    if (chdir(tmp_av[1]) != 0)
+                    {
+                        perror("cd");
+                        last_exit_status = 1;
+                    }
+                }
+            }
+            else if (strcmp(tmp_av[0], "env") == 0)
+            {
+                print_environment(environ);
+            }
+        }
+        else
+        {
+            char **argv = tmp_av;
+
+            int pipefd[2];
+            if (pipe(pipefd) == -1)
+            {
+                perror("pipe");
+                last_exit_status = 1;
+                continue;
+            }
+
+            pid = fork();
+            if (pid < 0)
+            {
+                perror("fork");
+                last_exit_status = 1;
+                exit(EXIT_FAILURE);
+            }
+            else if (pid == 0)
+            {
+                close(pipefd[0]);
+
+                if (dup2(pipefd[1], STDOUT_FILENO) == -1)
+                {
+                    perror("dup2");
+                    last_exit_status = 1;
+                    exit(EXIT_FAILURE);
+                }
+
+                close(pipefd[1]);
+
+                if (execvp(argv[0], argv) == -1)
+                {
+                    perror("execvp");
+                    last_exit_status = 127;
+                    exit(EXIT_FAILURE);
+                }
+            }
+            else
+            {
+                close(pipefd[1]);
+
+                while ((bytesRead = read(pipefd[0], buffer, sizeof(buffer))) > 0)
+                {
+                    write(STDOUT_FILENO, buffer, bytesRead);
+                }
+
+                close(pipefd[0]);
+
+                if (waitpid(pid, &status, 0) == -1)
+                {
+                    perror("waitpid");
+                    last_exit_status = 1;
+                }
+                else
+                {
+                    last_exit_status = WIFEXITED(status) ? WEXITSTATUS(status) : 1;
+                }
+            }
+        }
+    }
+    return last_exit_status;
 }
+
