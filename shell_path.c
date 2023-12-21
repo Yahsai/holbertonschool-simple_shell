0e8235b713ec83b0f409e007d45d93571e9338b1
Yahsai <yahsaisantana1@gmail.com>
2023-12-21 05:09:56 -0400

    update

--- shell_path.c
+++ shell_path.c
@@ -14,10 +14,10 @@ void execute_command(char *command) {
         perror("fork");
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
-        // Child process
+        /* Child process */
         execlp(command, command, NULL);
 
-        // If execlp fails
+        /* If execlp fails */
         perror("exec");
         exit(EXIT_FAILURE);
     } else {
@@ -33,46 +33,45 @@ void set_path(char *path) {
 int main() {
     char input[MAX_INPUT_SIZE];
 
-    // Test cases
-    // TODO: Add more cases based on your specific requirements
+    /* Test cases */
+    /* TODO: Add more cases based on your specific requirements */
 
-    // Execute ls
+    /* Execute ls */
     execute_command("/bin/ls");
 
-    // Copy the file /bin/ls to hbtn_ls and execute ./.././../hbtn_ls /var
+    /* Copy the file /bin/ls to hbtn_ls and execute ./.././../hbtn_ls /var*/
     system("cp /bin/ls hbtn_ls");
     set_path("./.././../");
     execute_command("hbtn_ls /var");
 
-    // Execute ls -l
+    /* Execute ls -l */
     execute_command("ls -l");
 
-    // ...
 
     while (1) {
-        // Display shell prompt
+        /*  Display shell prompt */
         printf("($) ");
         fflush(stdout);
 
-        // Read user input
+        /* Read user input */
         if (fgets(input, sizeof(input), stdin) == NULL) {
-            // Handle Ctrl+D or other errors
+            /* Handle Ctrl+D or other errors */
             break;
         }
 
-        // Remove newline character
+        /* Remove newline character */
         input[strcspn(input, "\n")] = '\0';
 
-        // Check for the "exit" command
+        /* Check for the "exit" command */
         if (strcmp(input, "exit") == 0) {
             break;
         }
 
-        // Execute the command
+        /* Execute the command */
         execute_command(input);
     }
 
-    printf("\n");  // Add a newline for better shell appearance
+    printf("\n");  /* Add a newline for better shell appearance */
     return 0;
 }
 
