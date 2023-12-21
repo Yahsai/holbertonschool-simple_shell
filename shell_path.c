00ff5319cfb4de749475ed9021ec1724d2d57c76
Yahsai <yahsaisantana1@gmail.com>
2023-12-21 05:14:32 -0400

    update

--- shell_path.c
+++ shell_path.c
@@ -7,6 +7,13 @@
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKENS 100
 
+void free_tokens(char **tokens) {
+    for (int i = 0; tokens[i] != NULL; ++i) {
+        free(tokens[i]);
+    }
+    free(tokens);
+}
+
 void execute_command(char *command) {
     pid_t pid = fork();
 
@@ -14,64 +21,73 @@ void execute_command(char *command) {
         perror("fork");
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
-        /* Child process */
+        // Child process
         execlp(command, command, NULL);
 
-        /* If execlp fails */
+        // If execlp fails
         perror("exec");
         exit(EXIT_FAILURE);
     } else {
-        /* Parent process */
+        // Parent process
         wait(NULL);
     }
 }
 
-void set_path(char *path) {
-    setenv("PATH", path, 1);
+char **tokenize_input(char *input) {
+    char **tokens = (char **)malloc(MAX_TOKENS * sizeof(char *));
+    if (!tokens) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    char *token = strtok(input, " ");
+    int token_count = 0;
+
+    while (token != NULL && token_count < MAX_TOKENS - 1) {
+        tokens[token_count++] = strdup(token);
+        token = strtok(NULL, " ");
+    }
+
+    tokens[token_count] = NULL;
+
+    return tokens;
 }
 
 int main() {
     char input[MAX_INPUT_SIZE];
 
-    /* Test cases */
-    /* TODO: Add more cases based on your specific requirements */
-
-    /* Execute ls */
-    execute_command("/bin/ls");
-
-    /* Copy the file /bin/ls to hbtn_ls and execute ./.././../hbtn_ls /var*/
-    system("cp /bin/ls hbtn_ls");
-    set_path("./.././../");
-    execute_command("hbtn_ls /var");
-
-    /* Execute ls -l */
-    execute_command("ls -l");
-
-
     while (1) {
-        /*  Display shell prompt */
+        // Display shell prompt
         printf("($) ");
         fflush(stdout);
 
-        /* Read user input */
+        // Read user input
         if (fgets(input, sizeof(input), stdin) == NULL) {
-            /* Handle Ctrl+D or other errors */
+            // Handle Ctrl+D or other errors
             break;
         }
 
-        /* Remove newline character */
+        // Remove newline character
         input[strcspn(input, "\n")] = '\0';
 
-        /* Check for the "exit" command */
+        // Check for the "exit" command
         if (strcmp(input, "exit") == 0) {
             break;
         }
 
-        /* Execute the command */
-        execute_command(input);
+        // Tokenize the input
+        char **tokens = tokenize_input(input);
+
+        if (tokens[0] != NULL) {
+            // Execute the command
+            execute_command(tokens[0]);
+
+            // Free the allocated memory for tokens
+            free_tokens(tokens);
+        }
     }
 
-    printf("\n");  /* Add a newline for better shell appearance */
+    printf("\n");  // Add a newline for better shell appearance
     return 0;
 }
 
