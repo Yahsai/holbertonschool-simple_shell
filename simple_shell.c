f801f210123665c5fc71a1103526785e6c7180ad
Yahsai <yahsaisantana1@gmail.com>
2023-12-20 21:51:15 -0400

    update

--- simple_shell.c
+++ simple_shell.c
@@ -55,6 +55,65 @@ void print_environment(char **env)
 }
 
 /**
+ * find_command - Find the full path of the command using PATH
+ * @command: The command to find
+ * @path: The PATH variable
+ * @full_path: Buffer to store the full path
+ *
+ * Return: 1 if the command is found, 0 otherwise
+ */
+int find_command(char *command, char *path, char *full_path)
+{
+    char *token;
+    char *path_copy;
+    char temp_path[MAX_COMMAND_LENGTH];
+
+    path_copy = strdup(path);
+    if (path_copy == NULL)
+    {
+        perror("strdup");
+        return 0;
+    }
+
+    token = strtok(path_copy, ":");
+    while (token != NULL)
+    {
+        snprintf(temp_path, sizeof(temp_path), "%s/%s", token, command);
+        if (access(temp_path, F_OK | X_OK) == 0)
+        {
+            strcpy(full_path, temp_path);
+            free(path_copy);
+            return 1;
+        }
+        token = strtok(NULL, ":");
+    }
+
+    free(path_copy);
+    return 0;
+}
+
+/**
+ * exit_shell - Exit the shell
+ *
+ * Return: Nothing
+ */
+void exit_shell(void)
+{
+    printf("Exiting shell\n");
+    exit(0);
+}
+
+/**
+ * env_shell - Print the current environment
+ *
+ * Return: Nothing
+ */
+void env_shell(void)
+{
+    print_environment(environ);
+}
+
+/**
  * main - Entry point for the shell
  *
  * Return: Exit status
@@ -69,6 +128,8 @@ int main(void)
     int ac;
     int has_token;
     int last_exit_status = 0;
+    char *path = getenv("PATH");
+    char full_path[MAX_COMMAND_LENGTH];
 
     ssize_t bytesRead;
     char buffer[1024];
@@ -112,11 +173,13 @@ int main(void)
 
         if (strcmp(tmp_av[0], "exit") == 0)
         {
-            last_exit_status = 0;
-            break;
+            exit_shell();
         }
-
-        if (is_builtin_command(tmp_av[0]))
+        else if (strcmp(tmp_av[0], "env") == 0)
+        {
+            env_shell();
+        }
+        else if (is_builtin_command(tmp_av[0]))
         {
             if (strcmp(tmp_av[0], "cd") == 0)
             {
@@ -129,70 +192,74 @@ int main(void)
                     }
                 }
             }
-            else if (strcmp(tmp_av[0], "env") == 0)
-            {
-                print_environment(environ);
-            }
         }
         else
         {
-            char **argv = tmp_av;
-
-            int pipefd[2];
-            if (pipe(pipefd) == -1)
+            if (find_command(tmp_av[0], path, full_path))
             {
-                perror("pipe");
-                last_exit_status = 1;
-                continue;
-            }
+                char **argv = tmp_av;
 
-            pid = fork();
-            if (pid < 0)
-            {
-                perror("fork");
-                last_exit_status = 1;
-                exit(EXIT_FAILURE);
-            }
-            else if (pid == 0)
-            {
-                close(pipefd[0]);
-
-                if (dup2(pipefd[1], STDOUT_FILENO) == -1)
+                int pipefd[2];
+                if (pipe(pipefd) == -1)
                 {
-                    perror("dup2");
+                    perror("pipe");
+                    last_exit_status = 1;
+                    continue;
+                }
+
+                pid = fork();
+                if (pid < 0)
+                {
+                    perror("fork");
                     last_exit_status = 1;
                     exit(EXIT_FAILURE);
                 }
-
-                close(pipefd[1]);
-
-                if (execvp(argv[0], argv) == -1)
+                else if (pid == 0)
                 {
-                    perror("execvp");
-                    last_exit_status = 127;
-                    exit(EXIT_FAILURE);
+                    close(pipefd[0]);
+
+                    if (dup2(pipefd[1], STDOUT_FILENO) == -1)
+                    {
+                        perror("dup2");
+                        last_exit_status = 1;
+                        exit(EXIT_FAILURE);
+                    }
+
+                    close(pipefd[1]);
+
+                    if (execv(full_path, argv) == -1)
+                    {
+                        perror("execv");
+                        last_exit_status = 127;
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                else
+                {
+                    close(pipefd[1]);
+
+                    while ((bytesRead = read(pipefd[0], buffer, sizeof(buffer))) > 0)
+                    {
+                        write(STDOUT_FILENO, buffer, bytesRead);
+                    }
+
+                    close(pipefd[0]);
+
+                    if (waitpid(pid, &status, 0) == -1)
+                    {
+                        perror("waitpid");
+                        last_exit_status = 1;
+                    }
+                    else
+                    {
+                        last_exit_status = WIFEXITED(status) ? WEXITSTATUS(status) : 1;
+                    }
                 }
             }
             else
             {
-                close(pipefd[1]);
-
-                while ((bytesRead = read(pipefd[0], buffer, sizeof(buffer))) > 0)
-                {
-                    write(STDOUT_FILENO, buffer, bytesRead);
-                }
-
-                close(pipefd[0]);
-
-                if (waitpid(pid, &status, 0) == -1)
-                {
-                    perror("waitpid");
-                    last_exit_status = 1;
-                }
-                else
-                {
-                    last_exit_status = WIFEXITED(status) ? WEXITSTATUS(status) : 1;
-                }
+                fprintf(stderr, "Command not found: %s\n", tmp_av[0]);
+                last_exit_status = 127;
             }
         }
     }
