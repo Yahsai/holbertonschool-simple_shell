93e7514525fb41e43e5eefee3c80df8ed23ec65d
Yahsai <yahsaisantana1@gmail.com>
2023-12-21 05:22:24 -0400

    update

--- simple_shell.c
+++ simple_shell.c
@@ -1,122 +1,165 @@
-#include "my_shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define MAX_COMMAND_LENGTH 100
+#define MAX_ARGS 10
+
+/* Additional functions if you have them */
 
 /**
- * get_full_path - Get the full path of a command
- * @command_name: The name of the command
- * Return: A dynamically allocated string containing the full path
- *         NULL if the command is not found
+ * is_builtin_command - determines whether given command is built-in or not
+ * @command: the given command
+ * Return: 0 if not built-in, 1 if built-in
  */
-char *get_full_path(char *command_name)
+int is_builtin_command(char *command)
 {
-    char *base_path = "/usr/bin/";
-    char *full_path;
-
-    if (access(command_name, X_OK) == 0)
-    {
-        full_path = strdup(command_name);
-        if (full_path == NULL)
-        {
-            perror("malloc");
-            return (NULL);
-        }
-        return (full_path);
-    }
-
-    full_path = malloc(strlen(base_path) + strlen(command_name) + 1);
-    if (full_path == NULL)
-    {
-        perror("malloc");
-        return (NULL);
-    }
-
-    sprintf(full_path, "%s%s", base_path, command_name);
-    if (access(full_path, X_OK) != 0)
-    {
-        perror("Command not found");
-        free(full_path);
-        return (NULL);
-    }
-
-    return (full_path);
+	if (strcmp(command, "exit") == 0 || strcmp(command, "clear") == 0 ||
+			strcmp(command, "cd") == 0 || strcmp(command, "env") == 0)
+	{
+		return 1;
+	}
+	return 0;
 }
 
 /**
- * Tokenize_execute - Tokenize a command string and execute it
- * @command_string: The command string to tokenize
- * @env: The environment variables
+ * print_environment - prints the environment variables
+ * @env: array of strings containing environment variables
+ * Return: nothing
  */
-void Tokenize_execute(char *command_string, char **env)
+void print_environment(char **env)
 {
-    char *token = NULL;
-    char **tokens = NULL;
-    int index = 0;
-    char *full_path;
-
-    token = strtok(command_string, " \n"); /* Tokenize the command string */
-    if (token == NULL)
-        return;
-
-    while (token != NULL)
-    {
-        tokens = realloc(tokens, sizeof(char *) * (index + 1));
-        if (tokens == NULL)
-        {
-            perror("realloc");
-            return;
-        }
-
-        tokens[index] = strdup(token); /* Store token in tokens array */
-        index++;                       /* Increment the token count */
-        token = strtok(NULL, " \n");   /* Get next token from command string */
-    }
-
-    tokens = realloc(tokens, sizeof(char *) * (index + 1));
-    if (tokens == NULL)
-    {
-        perror("realloc");
-        return;
-    }
-
-    tokens[index] = NULL;                 /* Set next element in the array to NULL */
-    full_path = get_full_path(tokens[0]); /* Get the full path of the command */
-    if (full_path == NULL)
-    {
-        free_tokens(tokens, index); /* Fix the function name */
-        return;
-    }
-
-    free(tokens[0]);
-    tokens[0] = full_path;
-    create_child_process(tokens, env);
-    free_tokens(tokens, index); /* Fix the function name */
+	while (*env != NULL)
+	{
+		printf("%s\n", *env);
+		env++;
+	}
 }
 
 /**
- * create_child_process - Create a child process and execute the command
- * @tokens: The tokenized command and arguments
- * @env: The environment variables
+ * prompt - returns to prompt. Used to ignore SIGINT
+ * @signo: the signal number
+ * Return: nothing
  */
-void create_child_process(char **tokens, char **env)
+void prompt(int signo)
 {
-    pid_t pid = fork(); /* Create a child process */
-    int status;
-
-    if (pid == -1)
-    {
-        perror("fork"); /* Print error message if fork fails */
-        return;
-    }
-
-    if (pid == 0)
-    {
-        execve(tokens[0], tokens, env); /* Execute the command using execve */
-        perror("error ");               /* Print an error message if execve fails */
-        exit(EXIT_FAILURE);             /* Exit child process with failure status */
-    }
-    else
-    {
-        /* Wait specifically for the child process to complete */
-        waitpid(pid, &status, 0);
-    }
+	(void)signo;  /* Ignore the unused parameter to avoid a warning */
+	printf("\n");
+	fflush(stdout);
 }
+int main(int ac, char **av, char **env)
+{
+	char input[MAX_COMMAND_LENGTH];
+	int status;
+	pid_t pid;
+	char *tmp_av[MAX_ARGS + 1];
+	char *token;
+	int has_token;
+	int last_exit_status = 0;
 
+	signal(SIGINT, prompt);
+
+	while (1)
+	{
+		fflush(stdout);
+
+		if (fgets(input, sizeof(input), stdin) == NULL)
+		{
+			break;
+		}
+
+		input[strcspn(input, "\n")] = '\0';
+
+		if (strlen(input) == 0)
+		{
+			continue;
+		}
+
+		token = strtok(input, " ");
+		ac = 0;
+		has_token = 0;
+
+		while (token != NULL && ac < MAX_ARGS)
+		{
+			tmp_av[ac++] = token;
+			has_token = 1;
+			token = strtok(NULL, " ");
+		}
+
+		if (!has_token)
+		{
+			continue;
+		}
+
+		tmp_av[ac] = NULL;
+
+		if (strcmp(tmp_av[0], "exit") == 0)
+		{
+			last_exit_status = 0;
+			break;
+		}
+
+		if (is_builtin_command(tmp_av[0]))
+		{
+			if (strcmp(tmp_av[0], "cd") == 0)
+			{
+				if (ac > 1)
+				{
+					if (chdir(tmp_av[1]) != 0)
+					{
+						perror("cd");
+						last_exit_status = 1;
+					}
+				}
+			}
+			else if (strcmp(tmp_av[0], "env") == 0)
+			{
+				print_environment(env);
+			}
+		}
+		else
+		{
+			int i;
+			for (i = 0; i < ac; i++)
+			{
+				av[i] = tmp_av[i];
+			}
+			av[ac] = NULL;
+
+			pid = fork();
+			if (pid < 0)
+			{
+				perror("fork");
+				last_exit_status = 1;
+				exit(EXIT_FAILURE);
+			}
+			else if (pid == 0)
+			{
+				if (execvp(av[0], av) == -1)
+				{
+					perror("execvp");
+					last_exit_status = 127;
+					exit(EXIT_FAILURE);
+				}
+			}
+			else
+			{
+				wait(&status);
+
+				if (WIFEXITED(status))
+				{
+					last_exit_status = WEXITSTATUS(status);
+				}
+				else
+				{
+					last_exit_status = 1;
+				}
+			}
+		}
+	}
+
+	return last_exit_status;
+}
